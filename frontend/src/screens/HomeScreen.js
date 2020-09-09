5529bc8692502d195cc46802b3fb512da684af33
yaren-gl <yardenglasberg@gmail.com>
2020-09-09 17:56:29 +0300

    Small logic changes

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -6,34 +6,39 @@ import axios from 'axios';
 function HomeScreen(props) {
   // Retrieve products from backend
   const [products, setProduct] = useState([]);
+  const [isAdmin, setAdmin] = useState([]);
+  
   let newProducts = [];
   // Runs once after rendering component
   useEffect(() => {
     const fetchData = async () => {
       const { data } = await axios.get("/api/products");
       setProduct(data);
+      //const isAdmin = await axios.get("/isAdmin");
+      const isAdmin = true;
+      setAdmin(isAdmin);
+      console.log("isAdmin");
     }
     fetchData();
-    return () => {};
-  },[newProducts]);
-  newProducts = window.arr ? window.arr.data : products;
+    return () =>{};
+  },[newProducts,isAdmin]);
+  const productLink = isAdmin ? "/updateProduct/" : "/product/";
+  console.log(productLink);
+  newProducts = window.arr ? window.arr.data : products; 
 
   return newProducts.length > 0 ? <ul className="products">
     {
       newProducts.map(product =>
       <li key={product._id} >
       <div className="product">
-      <Link to={'/product/' + product._id}>
+      <Link to={productLink + product._id}>
                   <img
                     className="product-image"
                     src={product.image}
                     alt="product"
                   />
                 </Link>
-        <div className="product-name">
-        
-        </div>
-        <div className="product-name">{product.name}</div>
+        <div className="product-name"><Link to={'/product/' + product._id}>{product.name}</Link> </div>
         <div className="product-brand">{product.brand}</div>
         <div className="product-price">₪{product.price}</div>
         <div className="product-type">{product.type}</div>
