5e4005f7f65892ee16ae45be9b4dda9a2b0356f9
david.levinov <david.levinov@appsflyer.com>
2020-09-10 03:55:09 +0300

    double check

--- backend/server.js
+++ backend/server.js
@@ -6,9 +6,11 @@ const bodyParser = require('body-parser');
 const session = require('express-session');
 const CryptoJS = require("crypto-js");
 const redisClient = redis.createClient();
-let RedisStore = require('connect-redis')(session)
 const async = require('async');
+const moment = require('moment');
+let RedisStore = require('connect-redis')(session)
 const serverError = 'Internal server error';
+const CryptoSalt = 'very secret stuff';
 
 app.set('trust proxy', 1);
 app.use(session({
@@ -33,39 +35,40 @@ app.use(bodyParser.json());
 
 // crypto functionaility for password storage
 function encrypter(data) {
-    return CryptoJS.AES.encrypt(JSON.stringify(data), 'very secrety this').toString();
+    return CryptoJS.AES.encrypt(JSON.stringify(data), CryptoSalt).toString();
 }
 function decrypter(ciphertext) {
-    let bytes = CryptoJS.AES.decrypt(ciphertext, 'very secrety this');
+    let bytes = CryptoJS.AES.decrypt(ciphertext, CryptoSalt);
     return JSON.parse(bytes.toString(CryptoJS.enc.Utf8));
 }
 
 // adding admin user on load
-redisClient.HSET("users", "admin", encrypter("admin"), (err, reply)=>{
-    if(!err) {console.log("added admin user successfully")}
+redisClient.HSET("users", "admin", encrypter("admin"), (err, reply) => {
+    if (!err) { console.log("added admin user successfully") }
 })
 
 //---------------------------- Get Requests ----------------------------
 
 app.get("/isadmin", (req, res) => {
     // if (!req.session.username){ res.status(403).send('forbidden, please login')}
-    if (req.session.username == "admin"){
+    if (req.session.username == "admin") {
         res.send(true)
-    } else { res.send(false)}
+    } else { res.send(false) }
 });
 
-app.get("/adminpanel", (req, res)=>{
-    redisClient.scan(0, "match", "*-log", (err, reply)=>{
-        if (err) { res.status(500).send(serverError); }
-        async.map(reply[1], (element)=>{
-            redisClient.HGETALL(element, (err, reply)=>{
-                if (err) { res.status(500).send(serverError); }
-                reply["user"] = element.substring(0, element.length - 4);
-                console.log(JSON.stringify(reply));
-                res.status(200).send(JSON.stringify(reply));
-            })
-        })
-    })
+app.get("/adminpanel", (req, res) => {
+    let result = {};
+    redisClient.scan(0, "match", '*-log', function (err, userNames) {
+        async.each(userNames[1], function (user, callback) {
+            redisClient.HGETALL(user, function (err, value) {
+                user = user.substring(0, user.length - 4);
+                result[user] = value;
+                callback(err);
+            });
+        }, function () {
+            res.send(JSON.stringify(result));
+        });
+    });
 })
 
 // Done
@@ -84,11 +87,11 @@ app.get("/api/cart", (req, res) => {
     redisClient.HGETALL(userName + "-cart", (err, reply) => {
         if (err) { res.status(500).send(serverError); }
         userCart = reply;
-        if (userCart){
+        if (userCart) {
             let addedProductIds = Object.keys(userCart);
             let results = allProducts.filter(allProducts => addedProductIds.includes(allProducts._id));
             for (var i in results) {
-                 results[i].quantity = userCart[results[i]._id];
+                results[i].quantity = userCart[results[i]._id];
             }
             console.log("in api cart");
             res.status(200).send(results);
@@ -127,6 +130,10 @@ app.post('/cart/:productId', (req, res) => {
     let product = req.params.productId;
     redisClient.hincrby(userName + "-cart", product, 1, (err, reply) => {
         if (err) { res.status(500).send(serverError) }
+        redisClient.HSET(req.session.username + "-log", moment().format(),
+            `add product #${req.params.productId} to cart`, (err, reply) => {
+                if (err) { res.status(500).send(serverError); }
+            });
         res.status(200).end();
     })
 });
@@ -157,37 +164,49 @@ app.post('/cart/remove/:productId', (req, res) => {
     // if (!req.session.username){ res.status(403).send('forbidden, please login')}
     let userName = req.session.username;
     let allProducts = data.products;
-        redisClient.hdel(userName + "-cart", req.params.productId, (err, reply) => {
+    redisClient.hdel(userName + "-cart", req.params.productId, (err, reply) => {
         if (err) { res.status(500).send(serverError) }
-         redisClient.HGETALL(userName + "-cart", (err, userCart) => {
+        redisClient.HGETALL(userName + "-cart", (err, userCart) => {
             if (err) { res.status(500).send(serverError); }
-            if (userCart){
+            if (userCart) {
                 let addedProductIds = Object.keys(userCart);
                 let results = allProducts.filter(allProducts => addedProductIds.includes(allProducts._id));
+                redisClient.HSET(req.session.username + "-log", moment().format(),
+                    `remove product #${req.params.productId} from cart`, (err, reply) => {
+                        if (err) { res.status(500).send(serverError); }
+                    });
                 res.status(200).send(results);
             } else {
                 res.status(200).send([]);
             }
         })
-         
+
     })
 });
 
 app.post('/signin', (req, res) => {
     redisClient.hget("users", req.body.username, (err, reply) => {
         if (err) { res.status(500).send(serverError); }
+        console.log(req.body)
         if (req.body.password == decrypter(reply)) {
             req.session.username = req.body.username;
+            redisClient.HSET(req.session.username + "-log", moment().format(), "logged in", (err, reply) => {
+                if (err) { res.status(500).send(serverError); }
+            });
+            if (req.body.remember){
+                req.session.cookie.maxAge = 365 * 24 * 60 * 60 * 1000; // 1 year
+            }
             res.status(200).end();
         } else {
-            res.status(404).send('User not found'); 
+            res.status(404).send('User not found');
         }
     })
 });
 
 app.post('/register', (req, res) => {
-    console.log(`User with username ${req.body.username} and password ${req.body.password} registered`);
+    // console.log(`User with username ${req.body.username} and password ${req.body.password} registered`);
     // check if username already exists
+    console.log(req.session)
     redisClient.HEXISTS("users", req.body.username, (err, reply) => {
         if (err) { res.status(500).send(serverError); }
         if (reply == 1) { res.status(409).end(); }
@@ -195,6 +214,9 @@ app.post('/register', (req, res) => {
     redisClient.hset("users", req.body.username, encrypter(req.body.password), (err, reply) => {
         if (err) { res.status(500).send(serverError); }
         req.session.username = req.body.username;
+        if (req.body.remember){
+            req.session.cookie.maxAge = 365 * 24 * 60 * 60 * 1000; // 1 year
+        }
         res.status(201).end();
     })
     console.log(req.body);
@@ -206,17 +228,18 @@ app.post('/checkout', (req, res) => {
     res.end();
 });
 
+// creates shipping value under user name table
 app.post('/updateShipping', (req, res) => {
     let userName = req.session.userame;
-    redisClient.HSET(userName, "shipping", JSON.stringify(req.body), (err)=>{
-        if(err) {res.status(500).send(serverError)}
+    redisClient.HSET(userName, "shipping", JSON.stringify(req.body), (err) => {
+        if (err) { res.status(500).send(serverError) }
     })
 })
 
 app.get('/getShipping', (req, res) => {
     let userName = req.session.userame;
-    redisClient.HGET(userName, "shipping", (err,reply)=>{
-        if(err) {res.status(500).send(serverError)}
+    redisClient.HGET(userName, "shipping", (err, reply) => {
+        if (err) { res.status(500).send(serverError) }
         res.status(200).send(JSON.parse(reply));
     })
 })
@@ -226,6 +249,10 @@ app.delete('/logout', (req, res) => {
     req.session.destroy((err) => {
         res.clearCookie('connect.sid', { path: '/' });
         console.log(`User ${userName} session killed`);
+        redisClient.HSET(userName + "-log", moment().format(),
+            `logged out`, (err, reply) => {
+                if (err) { res.status(500).send(serverError); }
+            });
         res.end();
     })
 });
